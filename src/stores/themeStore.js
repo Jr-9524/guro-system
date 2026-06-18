2cf77b8b315e9106de6080ef39f361765c55293a
Jr-9524 <jhonruzzelolan2@gmail.com>
2026-06-18 23:34:44 +0800

    fix: complete GURO theme store export

--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -11,24 +11,3 @@ const getSavedTheme = () => {
 
 const useThemeStore = create((set, get) => ({
   currentTheme: getSavedTheme(),
-
-  themeCategories: {
-    Appearance: themes,
-  },
-
-  setTheme: (theme) => {
-    const nextTheme = themes.includes(theme) ? theme : defaultTheme;
-    applyTheme(nextTheme);
-    localStorage.setItem("app-theme", nextTheme);
-    set({ currentTheme: nextTheme });
-  },
-
-  initTheme: () => {
-    const savedTheme = getSavedTheme();
-    applyTheme(savedTheme);
-    localStorage.setItem("app-theme", savedTheme);
-    set({ currentTheme: savedTheme });
-  },
-
-  getCurrentTheme: () => get().currentTheme,
-}));
